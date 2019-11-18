4dff883a0d01340280c6a485ca8065013e20b933
dkiit07 <dk.iit07@gmail.com>
2019-11-17 17:49:27 -0700

    uci wine quality dataset

--- src/wine-quality-dataset/quality_prediction.py
+++ src/wine-quality-dataset/quality_prediction.py
@@ -2,6 +2,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import csv
 from src.forwardSelection import forward_feature_selection
+from src.backwardSelection import backward_feature_selection_p_value
 from utils.helper_utils import prepare_wine_quality_data, prepare_wine_quality_test_data
 
 
@@ -16,11 +17,14 @@ def write_solution_to_file(ids, y_test):
             my_file_writer.writerow([int(id), y])
 
 
-def prediction():
+def prediction(selection_method):
 
     X_train, X_test, y_train, y_test = prepare_wine_quality_data()
 
-    feature_set = forward_feature_selection(X_train, X_test, y_train, y_test, 8)
+    if selection_method == 'forward':
+        feature_set = forward_feature_selection(X_train, X_test, y_train, y_test, 8)
+    elif selection_method == 'backward':
+        feature_set = backward_feature_selection_p_value(X_train, X_test, y_train, y_test, 8)
 
     X_test, ids = prepare_wine_quality_test_data();
 
@@ -36,7 +40,10 @@ def prediction():
 
 
 def main():
-    ids, y_test = prediction()
+    # ids, y_test = prediction(selection_method='forward')
+    # write_solution_to_file(ids, y_test)
+
+    ids, y_test = prediction(selection_method='backward')
     write_solution_to_file(ids, y_test)
 
 
